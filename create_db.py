a0e96ed17d6628889286cc60821d3c292f2a9169
IvonaTau <ivona.tautkute@gmail.com>
2017-10-16 12:17:06 +0200

    bug fix for images that cannot be read

--- create_db.py
+++ create_db.py
@@ -50,8 +50,10 @@ def main():
 
         if np.isnan(gender[i]):
             continue        
-        
-        img = cv2.imread(root_path + str(full_path[i][0]))
+        try:
+            img = cv2.imread(root_path + str(full_path[i][0]))
+        except FileNotFoundError:
+            print(root_path + str(full_path[i][0]), 'image is missing')
         try:
             out_imgs.append(cv2.resize(img, (img_size, img_size)))
             out_genders.append(int(gender[i]))
